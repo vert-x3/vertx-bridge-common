cb01df01f04e008687b2455926d4ca026b73a105
Clement Escoffier <clement.escoffier@gmail.com>
2015-11-27 18:00:27 +0100

    Extract bridge configuration to a separated artifact.

---
+++ src/test/java/io/vertx/ext/bridge/inheritance/MyBridgeOptions.java
@@ -0,0 +1,46 @@
+package io.vertx.ext.bridge.inheritance;
+
+import io.vertx.codegen.annotations.DataObject;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.bridge.BridgeOptions;
+import io.vertx.ext.bridge.PermittedOptions;
+
+import java.util.List;
+
+/**
+ * @author <a href="http://escoffier.me">Clement Escoffier</a>
+ */
+@DataObject(generateConverter = true, inheritConverter = true)
+public class MyBridgeOptions extends BridgeOptions {
+
+  private boolean flag;
+
+  public MyBridgeOptions() {
+
+  }
+
+  public MyBridgeOptions(MyBridgeOptions that) {
+    super(that);
+    this.flag = that.flag;
+  }
+
+  public MyBridgeOptions(JsonObject json) {
+    MyBridgeOptionsConverter.fromJson(json, this);
+  }
+
+  public JsonObject toJson() {
+    JsonObject json = new JsonObject();
+    MyBridgeOptionsConverter.toJson(this, json);
+    return json;
+  }
+
+  public boolean isFlag() {
+    return flag;
+  }
+
+  public MyBridgeOptions setFlag(boolean flag) {
+    this.flag = flag;
+    return this;
+  }
+
+}
